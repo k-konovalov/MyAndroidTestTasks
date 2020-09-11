719b23209c7a07b5b305574410c8da6062377299
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-11 18:06:38 +0300

    Add blank UI fragments

---
+++ fingerprint_guess_it_game/src/main/java/com/konovalovk/techno/guessit/fragments/AuthFragment.kt
@@ -0,0 +1,6 @@
+package com.konovalovk.techno.guessit.fragments
+
+import androidx.fragment.app.Fragment
+import com.konovalovk.techno.guessit.R
+
+class AuthFragment : Fragment(R.layout.fragment_auth){}
---
+++ fingerprint_guess_it_game/src/main/java/com/konovalovk/techno/guessit/fragments/GameFragment.kt
@@ -0,0 +1,6 @@
+package com.konovalovk.techno.guessit.fragments
+
+import androidx.fragment.app.Fragment
+import com.konovalovk.techno.guessit.R
+
+class GameFragment : Fragment(R.layout.fragment_game)
