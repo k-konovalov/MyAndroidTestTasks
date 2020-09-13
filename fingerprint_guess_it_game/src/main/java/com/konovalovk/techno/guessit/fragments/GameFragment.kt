a79cc04a81748046f868ba30a0d52e197ca5904a
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-13 12:40:19 +0300

    Draft logic for gameFragment

--- fingerprint_guess_it_game/src/main/java/com/konovalovk/techno/guessit/fragments/GameFragment.kt
+++ fingerprint_guess_it_game/src/main/java/com/konovalovk/techno/guessit/fragments/GameFragment.kt
@@ -2,20 +2,40 @@ package com.konovalovk.techno.guessit.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
 import com.konovalovk.techno.guessit.R
 import com.konovalovk.techno.guessit.adapter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.fragment_game.*
+import kotlin.random.Random
 
 class GameFragment : Fragment(R.layout.fragment_game){
+    var guessInt = Random.nextInt(0, 9999)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        vpFirstDigit.adapter = ViewPagerAdapter()
-        vpFirstDigit.orientation = ViewPager2.ORIENTATION_VERTICAL
-        vpSecondDigit.adapter = ViewPagerAdapter()
+        val listOfViewPagers = listOf(vpFirstDigit, vpSecondDigit, vpThirdDigit, vpFourthDigit)
+        listOfViewPagers.forEach {
+            it.adapter = ViewPagerAdapter()
+            it.setOnScrollChangeListener { view, i, i2, i3, i4 ->
+                checkAnswer()
+            }
+        }
 
-        vpThirdDigit.adapter = ViewPagerAdapter()
-        vpFourthDigit.adapter = ViewPagerAdapter()
+        btnNewValue.setOnClickListener {
+            guessInt = Random.nextInt(0, 9999)
+        }
+    }
+
+    private fun checkAnswer(){
+        val userAnswer = "${vpFirstDigit.currentItem}${vpSecondDigit.currentItem}${vpThirdDigit.currentItem}${vpFourthDigit.currentItem}".toInt()
+        var msg = "Answer is "
+        msg += when {
+            userAnswer > guessInt -> "greater than stored value"
+            userAnswer < guessInt -> "less than stored value"
+            else -> "you're right!"
+        }
+
+        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
     }
 }
